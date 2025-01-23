8b79d8f5b8f31e0b7b807e471ecd9e126c611c1b
AbrahamRJ04 <abrahammjimenez@hotmail.com>
2025-01-22 23:24:24 -0600

    feat: Se incluye funcionalidad Back para el modulo Loggin

--- CafePulse.FrontClass/Controllers/LogginController.cs
+++ CafePulse.FrontClass/Controllers/LogginController.cs
@@ -11,6 +11,36 @@ namespace CafePulse.FrontClass.Controllers
             CafePulse.ModelClass.Usuario user = new ModelClass.Usuario();
             return View(user);
         }
-       
+
+        [HttpPost]
+        public IActionResult GetInto(CafePulse.ModelClass.Usuario user)
+        {
+            string User = Request.Form["Usuario"].ToString();
+            string Pass = Request.Form["Contraseña"].ToString();
+
+            ModelClass.Result response = CafePulse.BusinessClass.Usuario.GetAuthenticationLog(User, Pass);
+
+
+            if (response.Success)
+            {
+                ModelClass.Usuario usuario = new ModelClass.Usuario();
+                usuario = (ModelClass.Usuario)response.Object;
+
+                if (usuario != null)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ViewBag.Mensaje = "El usuario o Contraseña ingresados no son validos o no existen";
+                }
+            }
+            else
+            {
+                ViewBag.Mensaje = "El Usuario y/o Contraseña son Incorrectos";
+            }
+
+            return View("Modal");
+        }
     }
 }
